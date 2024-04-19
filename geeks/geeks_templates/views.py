af77c6a949c8ad57e3d22281032d52c876ce2637
EilaSagilova <example@example.com>
2024-04-19 11:52:03 +0500

    Init commit

---
+++ geeks/geeks_templates/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.shortcuts import render 
+   
+
+# create a function 
+def geeks_view(request): 
+    # create a dictionary to pass 
+    # data to the template 
+    context ={ 
+        "data":"Gfg is the best", 
+        "list":[1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 
+    } 
+
+    return render(request, "geeks.html", context) 
